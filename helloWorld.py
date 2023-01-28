d4dd8e29f42f0eae437074c7391cadf574396c07
Jamiu Adewale Yusuf <jamiuadewaleyusuf@gmail.com>
2023-01-28 19:29:59 +0100

    starting my python learning

---
+++ helloWorld.py
@@ -0,0 +1,2 @@
+print("My name is Adewale and this is my first Python file")
+print("HELLO WORLD!!!!!!!")
---
+++ task1.py
@@ -0,0 +1,30 @@
+print("We check in a patient named John Smith \n He's 20 years old and is a new patient")
+print("\n")
+print("TASK: Define three variables for \n 1. nae\n 2. age \n 3. check if he's a new or existing patient ")
+print("\n")
+print("My solution:")
+name = "John Smith"
+age = 20
+isNew = True
+
+print("The patient name is: " + name)
+print("The patient is ",  age, 'years old')
+print(isNew)
+if(isNew):
+    print(name + "is a new patient")
+else: print(name + "is an old patient")
+
+print("Make the question a structured")
+getName = input("what's your name? ")
+getAge = input("Enter your age: ")
+gender = input("Are you a male or female? ")
+status = input("Are you a new or old patient: ")
+
+if(gender == "male"):
+    gender = "he "
+elif (gender == "female"):
+    gender = "she "
+else: print("(Enter male or female as your gender)")
+
+print("\n")
+print("We check in a patient named " + getName + ", \n" + gender + " is ",  getAge, "years old and is a/an " + status + " patient")
